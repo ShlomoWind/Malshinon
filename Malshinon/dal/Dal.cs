8136eea9901b5fa1aadcb8a5c8f6b40c20e76428
Shlomo <sh.z.wind@gmail.com>
2025-06-09 15:54:48 +0300

    create class dal

---
+++ Malshinon/dal/Dal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Malshinon.dal
+{
+    internal class Dal
+    {
+        //Establishes the connection with the database
+        private string connStr = "server=localhost;username=root;password=;database=malshinon";
+        private MySqlConnection Conn;
+        public Dal()
+        {
+            this.Conn = new MySqlConnection(this.connStr);
+        }
+    }
+}
