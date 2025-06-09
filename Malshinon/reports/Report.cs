943e4954d3512c1086c3e0f77fdea742ed5aaeb4
Shlomo <sh.z.wind@gmail.com>
2025-06-09 15:30:09 +0300

    create class report

---
+++ Malshinon/reports/Report.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.reports
+{
+    internal class Report
+    {
+        public int id { get; }
+        public int reporter_id { get; }
+        public int target_id{ get; }
+        public string text { get; }
+        public string timestamp { get; }
+        public Report(int id, int reporter_id, int target_id, string text, string timestamp)
+        {
+            this.id = id;
+            this.reporter_id = reporter_id;
+            this.target_id = target_id;
+            this.text = text;
+            this.timestamp = timestamp;
+        }
+    }
+}
